fa3c41946241c1bcf313707662a25b32e6ac178a
ksyu-zait <60782178+ksyu-zait@users.noreply.github.com>
2020-05-13 15:49:43 +0800

    Add files via upload
    test system (very very very bad omg)

---
+++ test.py
@@ -0,0 +1,19 @@
+
+from numpy import loadtxt
+import tensorflow as tf
+import h5py
+import cv2
+model = tf.keras.models.load_model('MNIST_model.h5')
+model.summary()
+#categories = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def prepare(filepath):
+    new_array = cv2.imread(filepath)
+    new_array = new_array.reshape(-1, 28, 28, 1)
+    new_array = new_array.astype('float32')
+    new_array = 255 - new_array
+    return new_array / 255.0
+
+
+prediction = model.predict([prepare('image.png')])
+print(prediction)  # will be a list in a list.
